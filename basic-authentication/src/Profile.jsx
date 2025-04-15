4cccf978dbe0ea8150999fe1592382b9a29103d6
kendra.jade <kindrai.kendra@gmail.com>
2025-04-15 11:23:42 -0500

    Changes made to main.jsx and Profile.jsx, debugging required! From Video...
    https://www.youtube.com/watch?v=6G_lAVlnkn8&ab_channel=PerryGovier

---
+++ basic-authentication/src/Profile.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react'
+import { fetchUserAttributes } from 'aws-amplify/auth'
+import { withAuthenticator } from 'aws-amplify/ui-react'
+import Container from './Container'
+
+function Profile({signOut, user}) {
+
+    useEffect(() => {
+        checkUser()
+    }, [])
+
+    const [currentUser, setCurrentUser] = useState({user})
+
+    async function checkUser() {
+        try {
+            const userAttributes = await fetchUserAttributes();
+            setCurrentSuser({...userAttributes, ...user})
+        } catch (err) { console.error('error: ', err) }
+    }
+    return (
+        <Container>
+            <h1>Profile</h1>
+            <h2>Username: {currentUser.username}</h2>
+            <h3>Email: {currentUser.email}</h3>
+            <h4>Phone: {currentUser.phone_number ?? 'unknown'}</h4>
+            <button onClick={signOut}>Sign Out</button>
+        </Container>
+    );
+}
+
+export default withAuthenticator(Profile)
--- basic-authentication/src/main.jsx
+++ basic-authentication/src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App.jsx'
+import Router from './Router'
+import 'antd/dist/reset.css'
+
+import { BrowserRouter } from 'react-router-dom'
+
+import { Amplify } from 'aws-amplify'
+import config from './aws-exports'
+Amplify.configure(config)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
   </StrictMode>,
 )
